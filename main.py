8c7c9c66efac4d0cedf820056c09eb625bb47760
Sophie Moustard <sophie.moustard@student.ecp.fr>
2017-04-01 20:33:43 +0200

    remove preproces

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 from preprocess import fer2013
 from models.vgg import basic
-from train import
+from train import train_without_augmentation
 
 if __name__ == '__main__':
-    '''Load data'''
+    '''Load & preprocess data data'''
     (X_train, Y_train), (X_test, Y_test), (X_validation, Y_validation) = fer2013()
-    '''Preprocess data'''
-    gaussian_train = gaussianFilter(X_train)
-    X_train_preprocessed = median_substraction(gaussian_train[0][0])
-    gaussian_validation = gaussianFilter(X_validation)
-    X_validation_preprocessed = median_substraction(gaussian_validation[0][0])
-    gaussian_test = gaussianFilter(X_test)
-    X_test_preprocessed = median_substraction(gaussian_test[0][0])
     '''Define model'''
     model = basic()
     '''Train model'''
